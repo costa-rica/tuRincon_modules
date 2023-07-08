9e3d9abc128b03886f071eaedd82ca165a4f76b1
costa-rica <nrodrig1@gmail.com>
2023-07-08 08:28:09 +0200

    changed CONFIG_TYPE to FLASK_CONFIG_TYPE;

--- tr01_modules/tr01_models/config.py
+++ tr01_modules/tr01_models/config.py
@@ -1,12 +1,12 @@
 import os
 from tr01_config import ConfigDev, ConfigProd, ConfigLocal
 
-if os.environ.get('CONFIG_TYPE')=='local':
+if os.environ.get('FLASK_CONFIG_TYPE')=='local':
     config = ConfigLocal()
     print('- tr01_models/config: Local')
-elif os.environ.get('CONFIG_TYPE')=='dev':
+elif os.environ.get('FLASK_CONFIG_TYPE')=='dev':
     config = ConfigDev()
     print('- tr01_models/config: Development')
-elif os.environ.get('CONFIG_TYPE')=='prod':
+elif os.environ.get('FLASK_CONFIG_TYPE')=='prod':
     config = ConfigProd()
     print('- tr01_models/config: Production')
